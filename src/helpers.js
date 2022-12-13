cfbdd0751009a58f57f353df3c5dad1c6eee492a
thumpus <elliotjwallis@gmail.com>
2022-12-13 13:52:54 -0500

    first commit

---
+++ src/helpers.js
@@ -0,0 +1,14 @@
+const choice = (items) => {
+  let i = Math.floor(Math.random() * items.length);
+  return items[i];
+}
+
+const remove = (items, item) => {
+    for (let i = 0; i < items.length; i++) {
+        if (items[i] === item) {
+          return [...items.slice(0, i), ...items.slice(i + 1)]
+        }
+      }
+}
+
+export { choice, remove }
